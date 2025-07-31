d8b3d904c7fbdab5c9f933c49f188db264e232dc
LucasK650 <lucask650@yahoo.com>
2025-07-31 12:59:07 -0400

    small changes
    changed test params to 700s and some other small stuff

--- src/App.java
+++ src/App.java
@@ -7,9 +7,6 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -18,19 +15,19 @@ import org.w3c.dom.NodeList;
 
 public class App {
 
-    private static final String readFileName = "C:/cncm/cncm.prm.xml";
-    private static final String writeFileName = "C:/Users/Lucas/Desktop/cncm.prm_testing.xml";
+    private static final String readFileName = "C:/old cncm/cncm.prm.xml";
+    private static final String writeFileName = "C:/cncm/cncm.prm.xml";
     public static ArrayList<String> paramValues = new ArrayList<>();
     public static ArrayList<Integer> paramsToCheck = new ArrayList<>();
     public static void main(String[] args) throws Exception {
-        paramsToCheck.add(0);
-        paramsToCheck.add(10);
-        paramsToCheck.add(120);
-        paramsToCheck.add(320);
-        paramsToCheck.add(359);
-        paramsToCheck.add(496);
-        paramsToCheck.add(856);
-        paramsToCheck.add(999);
+        paramsToCheck.add(700);
+        paramsToCheck.add(710);
+        paramsToCheck.add(720);
+        paramsToCheck.add(725);
+        paramsToCheck.add(738);
+        paramsToCheck.add(750);
+        paramsToCheck.add(775);
+        paramsToCheck.add(792);
         //1. Document builder factory
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         //2. Document builder
@@ -53,16 +50,17 @@ public class App {
             String index = element.getAttribute("index");
             String text = element.getTextContent();
             if (paramsToCheck.contains(i)) {
-                writeNode.setTextContent(text);
+                writeNode.setTextContent("69");
             }
         }
-
+        System.out.println("Param values set");
         //Write to the new xml file
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         DOMSource source = new DOMSource(writeDocument);
         StreamResult result = new StreamResult(new File(writeFileName));
         transformer.transform(source, result);
+        System.out.println("Param values written");
 
     }
 }
