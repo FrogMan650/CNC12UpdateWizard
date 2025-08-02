092bfa6edea1f3157c5771088109d9694caa82ae
LucasK650 <lucask650@yahoo.com>
2025-08-01 20:47:26 -0400

    xml files and license done
    Finished up basic checks for xml files and transfer of license file. xml file checks can be made far better with specific situation checks. params need to be checked and set

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,6 +13,7 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -21,11 +25,15 @@ public class App {
     private static final String newCfgFile = "C:/cncm/cncmcfg.xml";
     private static final String oldWizardSettingsFile = "C:/old cncm/wizardsettings.xml";
     private static final String newWizardSettingsFile = "C:/cncm/wizardsettings.xml";
+    private static final String oldOptionsFile = "C:/old cncm/resources/vcp/options.xml";
+    private static final String newOptionsFile = "C:/cncm/resources/vcp/options.xml";
     public static ArrayList<String> paramValues = new ArrayList<>();
     public static ArrayList<Integer> paramsToCheck = new ArrayList<>();
     public static String board;
-    public static String versionRaw;
-    public static double versionCombined;
+    public static String oldversionRaw;
+    public static String newversionRaw;
+    public static double oldversionCombined;
+    public static double newversionCombined;
     public static void main(String[] args) throws Exception {
         paramsToCheck.add(700);
         paramsToCheck.add(710);
@@ -35,11 +43,14 @@ public class App {
         paramsToCheck.add(750);
         paramsToCheck.add(775);
         paramsToCheck.add(792);
-        //1. Document builder factory
+
+        //Document builder factory
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        //2. Document builder
+
+        //Document builder
         DocumentBuilder builder = factory.newDocumentBuilder();
-        //3. Parse the xml document
+
+        //Parse the xml document
         Document oldParmDocument = builder.parse(new File(oldParmFile));
         Document newParmDocument = builder.parse(new File(newParmFile));
 
@@ -49,7 +60,10 @@ public class App {
         Document oldWizardSettingsDocument = builder.parse(new File(oldWizardSettingsFile));
         Document newWizardSettingsDocument = builder.parse(new File(newWizardSettingsFile));
 
-        //4. Get root element
+        Document oldOptionsDocument = builder.parse(new File(oldOptionsFile));
+        Document newOptionsDocument = builder.parse(new File(newOptionsFile));
+
+        //Get root elements
         Element oldParmRootElement = oldParmDocument.getDocumentElement();
         Element newParmRootElement = newParmDocument.getDocumentElement();
 
@@ -59,53 +73,122 @@ public class App {
         Element oldWizardSettingsRootElement = oldWizardSettingsDocument.getDocumentElement();
         Element newWizardSettingsRootElement = newWizardSettingsDocument.getDocumentElement();
 
-        //Get board and software version
-        NodeList softwareVersionNodeList = oldParmRootElement.getElementsByTagName("SoftwareVersion");
-        String softwareVersion = softwareVersionNodeList.item(0).getTextContent();
-        String[] softwareVersionSplit = softwareVersion.split(" ");
-        board = softwareVersionSplit[0];
-        versionRaw = softwareVersionSplit[3];
-        String[] versionSplitSplit = versionRaw.split("\\.");
-        if (versionSplitSplit.length == 2) {
-            versionCombined = Double.parseDouble(versionSplitSplit[0] + versionSplitSplit[1]);
+        Element oldOptionsRootElement = oldOptionsDocument.getDocumentElement();
+        Element newOptionsRootElement = newOptionsDocument.getDocumentElement();
+
+        //Get board and software versions
+        NodeList oldsoftwareVersionNodeList = oldParmRootElement.getElementsByTagName("SoftwareVersion");
+        NodeList newsoftwareVersionNodeList = newParmRootElement.getElementsByTagName("SoftwareVersion");
+        String oldsoftwareVersion = oldsoftwareVersionNodeList.item(0).getTextContent();
+        String newsoftwareVersion = newsoftwareVersionNodeList.item(0).getTextContent();
+        String[] oldsoftwareVersionSplit = oldsoftwareVersion.split(" ");
+        String[] newsoftwareVersionSplit = newsoftwareVersion.split(" ");
+        board = oldsoftwareVersionSplit[0];
+        oldversionRaw = oldsoftwareVersionSplit[3];
+        newversionRaw = newsoftwareVersionSplit[3];
+        String[] oldversionSplitSplit = oldversionRaw.split("\\.");
+        String[] newversionSplitSplit = newversionRaw.split("\\.");
+        if (oldversionSplitSplit.length == 2) {
+            oldversionCombined = Double.parseDouble(oldversionSplitSplit[0] + oldversionSplitSplit[1]);
         } else {
-            versionCombined = Double.parseDouble(versionSplitSplit[0] + versionSplitSplit[1] + "." + versionSplitSplit[2]);
+            oldversionCombined = Double.parseDouble(oldversionSplitSplit[0] + oldversionSplitSplit[1] + "." + oldversionSplitSplit[2]);
         }
-        System.out.println(versionCombined);
+        if (newversionSplitSplit.length == 2) {
+            newversionCombined = Double.parseDouble(newversionSplitSplit[0] + newversionSplitSplit[1]);
+        } else {
+            newversionCombined = Double.parseDouble(newversionSplitSplit[0] + newversionSplitSplit[1] + "." + newversionSplitSplit[2]);
+        }
+        System.out.println("Board: " + board);
+        System.out.println("New version: " + oldversionRaw);
+        System.out.println("Old version: " + newversionRaw);
 
-        // //5. Get node list
-        // NodeList oldParmNodeList = oldParmRootElement.getElementsByTagName("value");
-        // NodeList newParmNodeList = newParmRootElement.getElementsByTagName("value");
-        // //6. Iterate through node list and get elements and attributes
-        // for(int i = 0; i < oldParmNodeList.getLength(); i ++) {
-        //     Node oldParmNode = oldParmNodeList.item(i);
-        //     Node newParmNode = newParmNodeList.item(i);
-        //     Element element = (Element) oldParmNode;
-        //     String index = element.getAttribute("index");
-        //     String text = element.getTextContent();
-        //     if (paramsToCheck.contains(i)) {
-        //         newParmNode.setTextContent("69");//replace 69 with text after testing
-        //     }
-        // }
-        // System.out.println("Param values set");
+        //License file
+        Files.copy(Paths.get("C:/old cncm/license.dat"), Paths.get("C:/cncm/license.dat"), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("License DONE");
 
-        // //Write to the xml files
-        // TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        // Transformer transformer = transformerFactory.newTransformer();
+        //Options file
+        NodeList oldOptionsNodeList = oldOptionsRootElement.getElementsByTagName("VcpOption");
+        NodeList newOptionsNodeList = newOptionsRootElement.getElementsByTagName("VcpOption");
+        for(int i = 0; i < oldOptionsNodeList.getLength(); i ++) {
+            for(int j = 0; j < newOptionsNodeList.getLength(); j ++) {
+                String oldOptionNodeText = oldOptionsNodeList.item(i).getChildNodes().item(1).getTextContent();
+                String newOptionNodeText = newOptionsNodeList.item(j).getChildNodes().item(1).getTextContent();
+                if (oldOptionNodeText.equals(newOptionNodeText)) {
+                    newOptionsNodeList.item(j).getChildNodes().item(3).setTextContent(oldOptionsNodeList.item(i).getChildNodes().item(3).getTextContent());
+                    break;
+                }
+            }
+        }
+        System.out.println("options.xml DONE");
 
-        // DOMSource parmDocSource = new DOMSource(newParmDocument);
-        // DOMSource cfgDocSource = new DOMSource(newCfgDocument);
-        // DOMSource wizardSettingsDocSource = new DOMSource(newWizardSettingsDocument);
+        //Wizard settings file
+        NodeList oldWizardSettingsNodeList = oldWizardSettingsRootElement.getChildNodes();
+        NodeList newWizardSettingsNodeList = newWizardSettingsRootElement.getChildNodes();
+        for (int i = 1; i < oldWizardSettingsNodeList.getLength(); i = i+2) {
+            for (int j = 1; j < newWizardSettingsNodeList.getLength(); j = j+2) {
+                if (oldWizardSettingsNodeList.item(i).toString().equals(newWizardSettingsNodeList.item(j).toString())) {
+                    Element element = (Element) oldWizardSettingsNodeList.item(i);
+                    Element element2 = (Element) newWizardSettingsNodeList.item(j);
+                    element2.setAttribute("value", element.getAttribute("value"));
+                    break;
+                }
+            }
+        }
+        System.out.println("wizardsettings.xml DONE");
 
-        // StreamResult parmDocResult = new StreamResult(new File(newParmFile));
-        // StreamResult cfgDocResult = new StreamResult(new File(newCfgFile));
-        // StreamResult wizardSettingsDocResult = new StreamResult(new File(newWizardSettingsFile));
+        //Config settings file
+        NodeList oldCfgNodeList = oldCfgRootElement.getChildNodes();
+        NodeList newCfgNodeList = newCfgRootElement.getChildNodes();
+        for (int i = 1; i < oldCfgNodeList.getLength(); i = i+2) {
+            Element element = (Element) oldCfgNodeList.item(i);
+            Element element2 = (Element) newCfgNodeList.item(i);
+            NamedNodeMap attributes = element.getAttributes();
+            NamedNodeMap attributes2 = element2.getAttributes();
+            for (int j = 0; j < attributes.getLength(); j++) {
+                for (int k = 0; k < attributes2.getLength(); k++) {
+                    if (attributes.item(j).getNodeName().equals(attributes2.item(k).getNodeName())) {
+                    element2.setAttribute(attributes2.item(k).getNodeName(), attributes.item(j).getTextContent());
+                    break;
+                    }
+                }
+            }
+        }
+        System.out.println("cncmcfg.xml DONE");
 
-        // transformer.transform(parmDocSource, parmDocResult);
-        // transformer.transform(cfgDocSource, cfgDocResult);
-        // transformer.transform(wizardSettingsDocSource, wizardSettingsDocResult);
+        //Parm values file
+        NodeList oldParmNodeList = oldParmRootElement.getElementsByTagName("value");
+        NodeList newParmNodeList = newParmRootElement.getElementsByTagName("value");
+        for(int i = 0; i < oldParmNodeList.getLength(); i ++) {
+            Node oldParmNode = oldParmNodeList.item(i);
+            Node newParmNode = newParmNodeList.item(i);
+            Element element = (Element) oldParmNode;
+            String text = element.getTextContent();
+            if (paramsToCheck.contains(i)) {
+                newParmNode.setTextContent(text);
+            }
+        }
+        System.out.println("cncm.prm.xml DONE");
 
-        // System.out.println("xml values written");
+        //Write to the xml files
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        DOMSource parmDocSource = new DOMSource(newParmDocument);
+        DOMSource cfgDocSource = new DOMSource(newCfgDocument);
+        DOMSource wizardSettingsDocSource = new DOMSource(newWizardSettingsDocument);
+        DOMSource optionsDocSource = new DOMSource(newOptionsDocument);
+
+        StreamResult parmDocResult = new StreamResult(new File(newParmFile));
+        StreamResult cfgDocResult = new StreamResult(new File(newCfgFile));
+        StreamResult wizardSettingsDocResult = new StreamResult(new File(newWizardSettingsFile));
+        StreamResult optionsDocResult = new StreamResult(new File(newOptionsFile));
+
+        transformer.transform(parmDocSource, parmDocResult);
+        transformer.transform(cfgDocSource, cfgDocResult);
+        transformer.transform(wizardSettingsDocSource, wizardSettingsDocResult);
+        transformer.transform(optionsDocSource, optionsDocResult);
+
+        System.out.println("ALL FILES SAVED");
 
     }
 }
